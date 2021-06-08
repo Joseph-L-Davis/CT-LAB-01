98e544d90d5a2c8b7f8021896780617657db8bbd
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-08 10:43:51 -0700

    capitalize and filter with array methods pass

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,10 +1,6 @@
 export default (arr) => {
-  const fOutArr = [];
-  for(let i = 0; i < arr.length ; i ++) {
-    arr[i] = arr[i].toUpperCase();
-    if(arr[i][0] !== 'F') {
-      fOutArr.push(arr[i]);
-    } 
-  }
-  return fOutArr;
+  const upperCaseArr = arr.map(item => item.toUpperCase());
+  const fOutStrings = upperCaseArr.filter(item => item[0] !== 'F');
+  return fOutStrings;
 };
+
